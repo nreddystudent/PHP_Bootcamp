83e4bb65d3e41908036c7bb0508457a34095ca8c
nreddystudent <nreddy@student.wethinkcode.co.za>
2019-10-08 17:26:11 +0200

    dooooone

--- day01/ex02/oddeven.php
+++ day01/ex02/oddeven.php
@@ -3,9 +3,7 @@
 	$f = fopen( 'php://stdin', 'r' );
 	echo "Enter a number: ";
 	$input = fgets($f);
-	str_replace("\n", '', $input);
-	echo $input."\n";
-	echo $input;
+	$input = str_replace("\n", '', $input);
 	if(is_numeric($input) == true){
 		if ($input%2 == 0){
 			echo "The number ".$input." is even\n";
@@ -14,7 +12,8 @@
 			echo "The number ".$input." is odd\n";
 		}
 	}
-	else{
-		echo "'".$input."'"." is not a number\n";
+	 else{
+	 	echo "'".$input."'"." is not a number\n";
 	}
+	fclose($f);
 ?>
