c0210e2d23c7253afc51760ec7476834d8d8d6ac
nreddystudent <nreddy@student.wethinkcode.co.za>
2019-10-11 12:03:32 +0200

    magni

---
+++ day02/ex02/magnifying_glass.php
@@ -0,0 +1,15 @@
+#!/usr/bin/php
+<?php
+	if ($argc < 2 || !file_exists($argv[1])){
+		exit;
+	}
+	$input = fopen($argv[1], 'r');
+	while (!feof($input))
+	{
+		$lines .= fgets($input);
+	}
+	$lines = preg_replace_callback('/(?<=\").*(?=\")/', function($lines){
+		return strtoupper($lines[0]);
+		}, $lines);
+	echo $lines;
+?>
