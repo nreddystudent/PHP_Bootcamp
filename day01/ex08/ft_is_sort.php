1aade2f19b01de2de4a354f80e080d511992e9c6
nreddystudent <nreddy@student.wethinkcode.co.za>
2019-10-11 09:30:27 +0200

    added stuff

--- day01/ex08/ft_is_sort.php
+++ day01/ex08/ft_is_sort.php
@@ -3,10 +3,13 @@
 	function ft_is_sort($input){
 		$temp = $input;
 		$index = 0;
+		$temp1 = $input;
 		sort($temp);
+		rsort($temp1);
 		foreach($input as $v){
-			if ($v != $temp[$index++])
+			if (($v != $temp[$index]) && ($v != $temp1[$index]))
 				return false;
+			$index++;
 		}
 		return true;
 	}
